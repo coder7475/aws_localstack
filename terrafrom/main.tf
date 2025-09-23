2ace63ba130a897b81671b937fc914c670317abe
coder7475 <robiulhossain7475@gmail.com>
2025-09-23 18:51:21 +0600

    feat(terraform): create vpc and subnet using terraform

--- terrafrom/main.tf
+++ terrafrom/main.tf
@@ -1,29 +1,23 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style           = true
-  endpoints {
-    s3 = "http://localhost:4566"
-  }
-}
-
 resource "aws_s3_bucket" "mybucket" {
   bucket = "day6-terraform-bucket"
   tags = {
     Environment = "Dev"
     Owner = "Day6"
+  }
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyTerraformVPC"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "MyTerraformSubnet"
   }
 }
